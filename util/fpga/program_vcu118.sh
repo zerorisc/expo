7eab64c8854acc59bf031b346170c93daed3ed06
Noah Moroze <noah@zerorisc.com>
2024-07-10 18:36:26 +0000

    [util/fpga] Add scripts for programming VCU118 bitstream
    Signed-off-by: Noah Moroze <noah@zerorisc.com>

---
+++ util/fpga/program_vcu118.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright zeroRISC Inc.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 </path/to/bitstream.bit>"
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+BITSTREAM=$1
+
+if command -v vivado; then
+  TOOL=vivado
+elif command -v vivado_lab; then
+  TOOL=vivado_lab
+else 
+  echo "Error: could not find tool. Please install Vivado or Vivado Lab Edition"
+  exit 1
+fi
+
+$TOOL -mode batch -nojournal -nolog -source "$SCRIPT_DIR/program_vcu118.tcl" -tclargs "$BITSTREAM"
---
+++ util/fpga/program_vcu118.tcl
@@ -0,0 +1,23 @@
+# Copyright zeroRISC Inc.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+if {[llength $argv] != 1} {
+    puts "expected -tclargs /path/to/bistream.bit"
+    exit 1
+}
+set bitstream [lindex $argv 0]
+
+open_hw_manager
+connect_hw_server -allow_non_jtag
+
+open_hw_target
+current_hw_device [get_hw_devices xcvu9p_0]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvu9p_0] 0]
+
+set_property PROBES.FILE {} [get_hw_devices xcvu9p_0]
+set_property FULL_PROBES.FILE {} [get_hw_devices xcvu9p_0]
+set_property PROGRAM.FILE $bitstream [get_hw_devices xcvu9p_0]
+program_hw_devices [get_hw_devices xcvu9p_0]
+
+refresh_hw_device [lindex [get_hw_devices xcvu9p_0] 0]
