1e9913985eb069f594e998d63c6186071b731f7a
James Wainwright <james.wainwright@lowrisc.org>
2025-08-21 13:57:17 +0100

    [sw] Use full path for generated register headers
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/device/lib/crypto/drivers/hmac.c
+++ sw/device/lib/crypto/drivers/hmac.c
@@ -16,7 +16,7 @@
 #include "sw/device/lib/crypto/drivers/entropy.h"
 #include "sw/device/lib/crypto/impl/status.h"
 
-#include "hmac_regs.h"  // Generated.
+#include "hw/top/hmac_regs.h"  // Generated.
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 // Module ID for status codes.
