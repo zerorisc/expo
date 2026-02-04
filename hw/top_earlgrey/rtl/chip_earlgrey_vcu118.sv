lowRISC/opentitan

commit 2376681d5f01fabc0e8a2732fdd0647687fe88ce

[bitstream] Collect MMI data into one file
Reduce BUILD file boilerplate by placing all memory map info in a single
file. Use a separate Processor element for each map, so the InstData
attribute can be used as a key to identify the memory to be updated.
This construction also permits multiple memories to be spliced in a
single call to updatemem, though that is not implemented in this commit.

Adjust MMI dump to use a regex for mem type and a dictionary for each
memory's parameters. For the mem type, some memories in the FPGA can
consist of both RAMB36 and RAMB18 types, depending on the width of the
array. The regex allows more flexible memory cell detection.

Rev up the bitstream manifest schema to v3 so the memories contained in
the MMI file are described. Add a function to collect MMI data from v2
cache entries and rewrite in the expected v3 format, with the MMI data
in a single file.

Finally, drop support for bitstream cache entries that do not contain a
manifest. The tools to create a manifest have been in the repo for
awhile, and as of this commit's creation, no such cache entries remain
in the public cloud storage bucket.

Signed-off-by: Alexander Williams <awill@opentitan.org>

hw/bitstream/vivado/BUILD
@@ -52,8 +52,7 @@ fusesoc_build(
     ],
     output_groups = {
         "bitstream": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw310_0.1.bit"],
-        "rom_mmi": ["synth-vivado/rom.mmi"],
-        "otp_mmi": ["synth-vivado/otp.mmi"],
+        "mmi": ["synth-vivado/memories.mmi"],
         "logs": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw310_0.1.runs/"],
     },
     systems = ["lowrisc:systems:chip_earlgrey_cw310"],
@@ -70,18 +69,10 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi",
+    name = "cw310_mmi",
     testonly = True,
     srcs = [":fpga_cw310"],
-    output_group = "rom_mmi",
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "otp_mmi",
-    testonly = True,
-    srcs = [":fpga_cw310"],
-    output_group = "otp_mmi",
+    output_group = "mmi",
     tags = ["manual"],
 )
 
@@ -102,8 +93,7 @@ fusesoc_build(
     ],
     output_groups = {
         "bitstream": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw310_hyperdebug_0.1.bit"],
-        "rom_mmi": ["synth-vivado/rom.mmi"],
-        "otp_mmi": ["synth-vivado/otp.mmi"],
+        "mmi": ["synth-vivado/memories.mmi"],
         "logs": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw310_hyperdebug_0.1.runs/"],
     },
     systems = ["lowrisc:systems:chip_earlgrey_cw310_hyperdebug"],
@@ -120,18 +110,10 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi_hyp",
+    name = "cw310_hyperdebug_mmi",
     testonly = True,
     srcs = [":fpga_cw310_hyperdebug"],
-    output_group = "rom_mmi",
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "otp_mmi_hyp",
-    testonly = True,
-    srcs = [":fpga_cw310_hyperdebug"],
-    output_group = "otp_mmi",
+    output_group = "mmi",
     tags = ["manual"],
 )
 
@@ -152,8 +134,7 @@ fusesoc_build(
     ],
     output_groups = {
         "bitstream": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw340_0.1.bit"],
-        "rom_mmi": ["synth-vivado/rom.mmi"],
-        "otp_mmi": ["synth-vivado/otp.mmi"],
+        "mmi": ["synth-vivado/memories.mmi"],
         "logs": ["synth-vivado/lowrisc_systems_chip_earlgrey_cw340_0.1.runs/"],
     },
     systems = ["lowrisc:systems:chip_earlgrey_cw340"],
@@ -170,18 +151,10 @@ filegroup(
 )
 
 filegroup(
-    name = "fpga_cw340_rom_mmi",
+    name = "cw340_mmi",
     testonly = True,
     srcs = [":fpga_cw340"],
-    output_group = "rom_mmi",
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "fpga_cw340_otp_mmi",
-    testonly = True,
-    srcs = [":fpga_cw340"],
-    output_group = "otp_mmi",
+    output_group = "mmi",
     tags = ["manual"],
 )
 
@@ -192,10 +165,11 @@ bitstream_manifest_fragment(
     srcs = [":fpga_cw310"],
     bitstream = "bitstream",
     design = "chip_earlgrey_cw310",
-    memory_maps = {
-        "rom_mmi": "rom",
-        "otp_mmi": "otp",
-    },
+    memories = [
+        "rom",
+        "otp",
+    ],
+    memory_map_file = ":cw310_mmi",
     tags = ["manual"],
 )
 
@@ -215,10 +189,11 @@ bitstream_manifest_fragment(
     srcs = [":fpga_cw310_hyperdebug"],
     bitstream = "bitstream",
     design = "chip_earlgrey_cw310_hyperdebug",
-    memory_maps = {
-        "rom_mmi": "rom",
-        "otp_mmi": "otp",
-    },
+    memories = [
+        "rom",
+        "otp",
+    ],
+    memory_map_file = ":cw310_hyperdebug_mmi",
     tags = ["manual"],
 )
 
@@ -238,10 +213,11 @@ bitstream_manifest_fragment(
     srcs = [":fpga_cw340"],
     bitstream = "bitstream",
     design = "chip_earlgrey_cw340",
-    memory_maps = {
-        "rom_mmi": "rom",
-        "otp_mmi": "otp",
-    },
+    memories = [
+        "rom",
+        "otp",
+    ],
+    memory_map_file = ":cw340_mmi",
     tags = ["manual"],
 )
 
@@ -260,9 +236,8 @@ pkg_files(
     name = "standard",
     testonly = True,
     srcs = [
+        ":cw310_mmi",
         ":fpga_cw310_test_rom",
-        ":otp_mmi",
-        ":rom_mmi",
     ],
     prefix = "earlgrey/fpga_cw310/standard",
     tags = ["manual"],
@@ -272,9 +247,8 @@ pkg_files(
     name = "hyperdebug",
     testonly = True,
     srcs = [
+        ":cw310_hyperdebug_mmi",
         ":fpga_cw310_test_rom_hyp",
-        ":otp_mmi_hyp",
-        ":rom_mmi_hyp",
     ],
     prefix = "earlgrey/fpga_cw310/hyperdebug",
     tags = ["manual"],

hw/top_earlgrey/BUILD
@@ -90,16 +90,15 @@ fpga_cw310(
     base = ":fpga_cw310",
     base_bitstream = "//hw/bitstream:bitstream",
     exec_env = "fpga_cw310_test_rom",
+    mmi = "//hw/bitstream:cw310_mmi",
     openocd_adapter_config = "//third_party/openocd:jtag_olimex_cfg",
     otp = "//hw/ip/otp_ctrl/data:img_rma",
-    otp_mmi = "//hw/bitstream:otp_mmi",
     param = {
         "interface": "cw310",
         "exit_success": DEFAULT_TEST_SUCCESS_MSG,
         "exit_failure": DEFAULT_TEST_FAILURE_MSG,
     },
     rom = "//sw/device/lib/testing/test_rom:test_rom",
-    rom_mmi = "//hw/bitstream:rom_mmi",
     test_cmd = """
         --exec="transport init"
         --exec="fpga load-bitstream {bitstream}"
@@ -156,15 +155,14 @@ fpga_cw310(
     ecdsa_key = {"//sw/device/silicon_creator/rom/keys/fake/ecdsa:test_key_0_ecdsa_p256": "test_key_0"},
     exec_env = "fpga_hyper310_rom_with_fake_keys",
     manifest = "//sw/device/silicon_creator/rom_ext:manifest",
+    mmi = "//hw/bitstream/hyperdebug:mmi",
     otp = "//hw/ip/otp_ctrl/data:img_rma",
-    otp_mmi = "//hw/bitstream/hyperdebug:otp_mmi",
     param = {
         "interface": "hyper310",
         "exit_success": DEFAULT_TEST_SUCCESS_MSG,
         "exit_failure": DEFAULT_TEST_FAILURE_MSG,
     },
     rom = "//sw/device/silicon_creator/rom:mask_rom",
-    rom_mmi = "//hw/bitstream/hyperdebug:rom_mmi",
     test_cmd = """
         --exec="transport init"
         --exec="fpga load-bitstream {bitstream}"
@@ -296,10 +294,9 @@ fpga_cw340(
     base = ":fpga_cw340",
     base_bitstream = "//hw/bitstream/cw340:bitstream",
     exec_env = "fpga_cw340_test_rom",
+    mmi = "//hw/bitstream/cw340:mmi",
     otp = "//hw/ip/otp_ctrl/data:img_rma",
-    otp_mmi = "//hw/bitstream/cw340:otp_mmi",
     rom = "//sw/device/lib/testing/test_rom:test_rom",
-    rom_mmi = "//hw/bitstream/cw340:rom_mmi",
     test_cmd = """
         --exec="transport init"
         --exec="fpga load-bitstream {bitstream}"
@@ -317,10 +314,9 @@ fpga_cw340(
     ecdsa_key = {"//sw/device/silicon_creator/rom/keys/fake/ecdsa:test_key_0_ecdsa_p256": "test_key_0"},
     exec_env = "fpga_cw340_rom_with_fake_keys",
     manifest = "//sw/device/silicon_creator/rom_ext:manifest",
+    mmi = "//hw/bitstream/cw340:mmi",
     otp = "//hw/ip/otp_ctrl/data:img_rma",
-    otp_mmi = "//hw/bitstream/cw340:otp_mmi",
     rom = "//sw/device/silicon_creator/rom:mask_rom",
-    rom_mmi = "//hw/bitstream/cw340:rom_mmi",
     test_cmd = """
         --exec="transport init"
         --exec="fpga load-bitstream {bitstream}"

hw/bitstream/BUILD
@@ -70,25 +70,13 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi",
+    name = "cw310_mmi",
     testonly = True,
     srcs = select({
         "bitstream_skip": ["//hw/bitstream/universal:none"],
-        "bitstream_vivado": ["//hw/bitstream/vivado:rom_mmi"],
-        "bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_rom_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_rom_mmi"],
-    }),
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "otp_mmi",
-    testonly = True,
-    srcs = select({
-        "bitstream_skip": ["//hw/bitstream/universal:none"],
-        "bitstream_vivado": ["//hw/bitstream/vivado:otp_mmi"],
-        "bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_otp_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_otp_mmi"],
+        "bitstream_vivado": ["//hw/bitstream/vivado:cw310_mmi"],
+        "bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_mmi"],
+        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_mmi"],
     }),
     tags = ["manual"],
 )
@@ -99,7 +87,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/lib/testing/test_rom:test_rom_fpga_cw310_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":cw310_mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -109,7 +98,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/silicon_creator/rom:mask_rom_fpga_cw310_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":cw310_mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -121,7 +111,8 @@ bitstream_splice(
         testonly = True,
         src = ":mask_rom",
         data = img_target,
-        meminfo = ":otp_mmi",
+        instance = "otp",
+        meminfo = ":cw310_mmi",
         tags = ["manual"],
         update_usr_access = True,
     )
@@ -133,8 +124,7 @@ bitstream_fragment_from_manifest(
     name = "chip_earlgrey_cw310_cached_fragment",
     srcs = [
         "@bitstreams//:chip_earlgrey_cw310_bitstream",
-        "@bitstreams//:chip_earlgrey_cw310_otp_mmi",
-        "@bitstreams//:chip_earlgrey_cw310_rom_mmi",
+        "@bitstreams//:chip_earlgrey_cw310_mmi",
     ],
     design = "chip_earlgrey_cw310",
     manifest = "@bitstreams//:manifest",
@@ -155,8 +145,7 @@ bitstream_fragment_from_manifest(
     name = "chip_earlgrey_cw310_hyperdebug_cached_fragment",
     srcs = [
         "@bitstreams//:chip_earlgrey_cw310_hyperdebug_bitstream",
-        "@bitstreams//:chip_earlgrey_cw310_hyperdebug_otp_mmi",
-        "@bitstreams//:chip_earlgrey_cw310_hyperdebug_rom_mmi",
+        "@bitstreams//:chip_earlgrey_cw310_hyperdebug_mmi",
     ],
     design = "chip_earlgrey_cw310_hyperdebug",
     manifest = "@bitstreams//:manifest",
@@ -177,8 +166,7 @@ bitstream_fragment_from_manifest(
     name = "chip_earlgrey_cw340_cached_fragment",
     srcs = [
         "@bitstreams//:chip_earlgrey_cw340_bitstream",
-        "@bitstreams//:chip_earlgrey_cw340_otp_mmi",
-        "@bitstreams//:chip_earlgrey_cw340_rom_mmi",
+        "@bitstreams//:chip_earlgrey_cw340_mmi",
     ],
     design = "chip_earlgrey_cw340",
     manifest = "@bitstreams//:manifest",

hw/bitstream/cw340/BUILD
@@ -25,25 +25,13 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi",
+    name = "mmi",
     testonly = True,
     srcs = select({
         "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:fpga_cw340_rom_mmi"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw340_rom_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw340_rom_mmi"],
-    }),
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "otp_mmi",
-    testonly = True,
-    srcs = select({
-        "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:fpga_cw340_otp_mmi"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw340_otp_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw340_otp_mmi"],
+        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:fpga_cw340_mmi"],
+        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw340_mmi"],
+        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw340_mmi"],
     }),
     tags = ["manual"],
 )
@@ -54,7 +42,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/lib/testing/test_rom:test_rom_fpga_cw340_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -65,7 +54,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/silicon_creator/rom:mask_rom_fpga_cw340_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -77,7 +67,8 @@ bitstream_splice(
         testonly = True,
         src = ":mask_rom",
         data = img_target,
-        meminfo = ":otp_mmi",
+        instance = "otp",
+        meminfo = ":mmi",
         tags = ["manual"],
         update_usr_access = True,
     )

hw/bitstream/hyperdebug/BUILD
@@ -21,25 +21,13 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi",
+    name = "mmi",
     testonly = True,
     srcs = select({
         "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:rom_mmi_hyp"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_rom_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_rom_mmi"],
-    }),
-    tags = ["manual"],
-)
-
-filegroup(
-    name = "otp_mmi",
-    testonly = True,
-    srcs = select({
-        "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:otp_mmi_hyp"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_otp_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_otp_mmi"],
+        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:cw310_hyperdebug_mmi"],
+        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_mmi"],
+        "//conditions:default": ["@bitstreams//:chip_earlgrey_cw310_hyperdebug_mmi"],
     }),
     tags = ["manual"],
 )
@@ -50,7 +38,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/lib/testing/test_rom:test_rom_fpga_cw310_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -61,7 +50,8 @@ bitstream_splice(
     testonly = True,
     src = ":bitstream",
     data = "//sw/device/silicon_creator/rom:mask_rom_fpga_cw310_scr_vmem",
-    meminfo = ":rom_mmi",
+    instance = "rom",
+    meminfo = ":mmi",
     tags = ["manual"],
     update_usr_access = True,
 )
@@ -73,7 +63,8 @@ bitstream_splice(
         testonly = True,
         src = ":mask_rom",
         data = img_target,
-        meminfo = ":otp_mmi",
+        instance = "otp",
+        meminfo = ":mmi",
         tags = ["manual"],
         update_usr_access = True,
     )

hw/bitstream/universal/BUILD
@@ -29,8 +29,8 @@ label_flag(
 universal_splice(
     name = "splice",
     testonly = True,
-    # When the src bitstream, rom_mmi and otp_mmi fields are empty,
-    # the rule will use values from the exec_env.
+    # When the src bitstream and mmi fields are empty, the rule will use values
+    # from the exec_env.
     exec_env = ":env",
     otp = ":otp",
     rom = ":rom",

sw/device/silicon_creator/rom/e2e/BUILD
@@ -70,7 +70,8 @@ bitstream_splice(
     name = "bitstream_default_otp",
     src = "//hw/bitstream:mask_rom",
     data = ":otp_img_default",
-    meminfo = "//hw/bitstream:otp_mmi",
+    instance = "otp",
+    meminfo = "//hw/bitstream:cw310_mmi",
     tags = maybe_skip_in_ci(CONST.LCV.RMA),
     update_usr_access = True,
     visibility = ["//visibility:private"],

sw/device/silicon_creator/rom/e2e/boot_policy_valid/BUILD
@@ -63,7 +63,8 @@ BOOT_POLICY_VALID_CASES = [
         name = "bitstream_boot_policy_valid_{}".format(lc_state),
         src = "//hw/bitstream:mask_rom",
         data = ":otp_img_boot_policy_valid_{}".format(lc_state),
-        meminfo = "//hw/bitstream:otp_mmi",
+        instance = "otp",
+        meminfo = "//hw/bitstream:cw310_mmi",
         update_usr_access = True,
     )
     for lc_state, _ in get_lc_items()

zerorisc/expo

commit 40771d4e0f8657e1ae17f67bfc880082d8ac68a1

[hw] Port MMI changes to VCU118
https://github.com/lowRISC/opentitan/commit/2376681d5f01fabc0e8a2732fdd0647687fe88ce
(h/t Noah) broke VCU118 builds, see #209.

Signed-off-by: Quan Nguyen <qmn@zerorisc.com>

hw/bitstream/vcu118/BUILD
@@ -2,7 +2,6 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-load("//rules:splice.bzl", "bitstream_splice")
 load("//rules:otp.bzl", "get_otp_images")
 
 # We do not have a signed VCU118 ROM yet.
@@ -25,61 +24,13 @@ filegroup(
 )
 
 filegroup(
-    name = "rom_mmi",
+    name = "mmi",
     testonly = True,
     srcs = select({
         "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:fpga_vcu118_rom_mmi"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_vcu118_rom_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_vcu118_rom_mmi"],
+        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:vcu118_mmi"],
+        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_vcu118_mmi"],
+        "//conditions:default": ["@bitstreams//:chip_earlgrey_vcu118_mmi"],
     }),
     tags = ["manual"],
 )
-
-filegroup(
-    name = "otp_mmi",
-    testonly = True,
-    srcs = select({
-        "//hw/bitstream:bitstream_skip": ["//hw/bitstream/universal:none"],
-        "//hw/bitstream:bitstream_vivado": ["//hw/bitstream/vivado:fpga_vcu118_otp_mmi"],
-        "//hw/bitstream:bitstream_gcp_splice": ["@bitstreams//:chip_earlgrey_vcu118_otp_mmi"],
-        "//conditions:default": ["@bitstreams//:chip_earlgrey_vcu118_otp_mmi"],
-    }),
-    tags = ["manual"],
-)
-
-# Build the Test ROM and splice it into the bitstream.
-bitstream_splice(
-    name = "test_rom",
-    testonly = True,
-    src = ":bitstream",
-    data = "//sw/device/lib/testing/test_rom:test_rom_fpga_vcu118_scr_vmem",
-    meminfo = ":rom_mmi",
-    tags = ["manual"],
-    update_usr_access = True,
-)
-
-# Build the ROM and splice it into a cached bitstream.
-bitstream_splice(
-    name = "mask_rom",
-    testonly = True,
-    src = ":bitstream",
-    data = "//sw/device/silicon_creator/rom:mask_rom_fpga_vcu118_scr_vmem",
-    meminfo = ":rom_mmi",
-    tags = ["manual"],
-    update_usr_access = True,
-)
-
-# Splice OTP images into the locally-spliced ROM bitstream.
-[
-    bitstream_splice(
-        name = "mask_rom_otp_{}".format(otp_name),
-        testonly = True,
-        src = ":mask_rom",
-        data = img_target,
-        meminfo = ":otp_mmi",
-        tags = ["manual"],
-        update_usr_access = True,
-    )
-    for (otp_name, img_target) in get_otp_images()
-]

hw/bitstream/vivado/BUILD
@@ -163,20 +163,19 @@ fusesoc_build(
     name = "fpga_vcu118",
     testonly = True,
     srcs = [
-        "//hw:all_files",
+        "//hw:rtl_files",
         _CW340_TESTROM,
         _OTP_RMA,
     ],
-    cores = ["//:cores"],
-    data = ["//hw/ip/otbn:all_files"],
+    cores = ["//hw:cores"],
+    data = ["//hw/ip/otbn:rtl_files"],
     flags = [
         "--BootRomInitFile=" + _CW340_TESTROM_PATH,
         "--OtpCtrlMemInitFile=" + _OTP_RMA_PATH,
     ],
     output_groups = {
         "bitstream": ["synth-vivado/lowrisc_systems_chip_earlgrey_vcu118_0.1.bit"],
-        "rom_mmi": ["synth-vivado/rom.mmi"],
-        "otp_mmi": ["synth-vivado/otp.mmi"],
+        "mmi": ["synth-vivado/memories.mmi"],
     },
     systems = ["lowrisc:systems:chip_earlgrey_vcu118"],
     tags = ["manual"],
@@ -298,16 +297,25 @@ pkg_tar(
     tags = ["manual"],
 )
 
+filegroup(
+    name = "vcu118_mmi",
+    testonly = True,
+    srcs = [":fpga_vcu118"],
+    output_group = "mmi",
+    tags = ["manual"],
+)
+
 bitstream_manifest_fragment(
     name = "earlgrey_vcu118_manifest",
     testonly = True,
     srcs = [":fpga_vcu118"],
     bitstream = "bitstream",
     design = "chip_earlgrey_vcu118",
-    memory_maps = {
-        "rom_mmi": "rom",
-        "otp_mmi": "otp",
-    },
+    memories = [
+        "rom",
+        "otp",
+    ],
+    memory_map_file = ":vcu118_mmi",
     tags = ["manual"],
 )
 

hw/top_earlgrey/BUILD
@@ -398,10 +398,9 @@ fpga_vcu118(
     base = ":fpga_vcu118",
     base_bitstream = "//hw/bitstream/vcu118:bitstream",
     exec_env = "fpga_vcu118_test_rom",
-    otp = "//hw/ip/otp_ctrl/data:img_rma",
-    #otp_mmi = "//hw/bitstream/vcu118:otp_mmi",
+    mmi = "//hw/bitstream/vcu118:mmi",
+    otp = "//hw/top_earlgrey/data/otp:img_rma",
     rom = "//sw/device/lib/testing/test_rom:test_rom",
-    #rom_mmi = "//hw/bitstream/vcu118:rom_mmi",
     test_cmd = """
         --exec="transport init"
         --exec="fpga load-bitstream {bitstream}"

hw/top_earlgrey/chip_earlgrey_vcu118.core
@@ -9,9 +9,9 @@ filesets:
     depend:
       - lowrisc:systems:top_earlgrey:0.1
       - lowrisc:systems:top_earlgrey_pkg
-      - lowrisc:systems:ast
-      - lowrisc:systems:padring
-      - lowrisc:systems:scan_role_pkg
+      - lowrisc:systems:top_earlgrey_ast
+      - lowrisc:systems:top_earlgrey_padring
+      - lowrisc:earlgrey_systems:scan_role_pkg
     files:
       - rtl/clkgen_vcu118.sv
       - rtl/usr_access_xil7series.sv

hw/top_earlgrey/rtl/chip_earlgrey_vcu118.sv
@@ -888,14 +888,7 @@ module chip_earlgrey_vcu118 #(
 
   prim_mubi_pkg::mubi4_t ast_init_done;
 
-  ast #(
-    .EntropyStreams(ast_pkg::EntropyStreams),
-    .AdcChannels(ast_pkg::AdcChannels),
-    .AdcDataWidth(ast_pkg::AdcDataWidth),
-    .UsbCalibWidth(ast_pkg::UsbCalibWidth),
-    .Ast2PadOutWidth(ast_pkg::Ast2PadOutWidth),
-    .Pad2AstInWidth(ast_pkg::Pad2AstInWidth)
-  ) u_ast (
+  ast u_ast (
     // external POR
     .por_ni                ( rst_n ),
 