3f77926e21b7640ac2182b0b1169000444decc02
Jade Philipoom <jadep@zerorisc.com>
2026-04-01 11:34:11 +0200

    fixup:add missing import

--- hw/ip/acc/util/shared/information_flow_analysis.py
+++ hw/ip/acc/util/shared/information_flow_analysis.py
@@ -4,6 +4,7 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+import re
 from copy import deepcopy
 from typing import Dict, List, Optional, Set, Tuple
 
