2933eedde11700be082f1f36160531aabb9c2d26
Kat Fox <kat@zerorisc.com>
2026-04-09 15:03:53 +0100

    [crypto] Standardize file header format for cryptolib and related files
    Signed-off-by: Kat Fox <kat@zerorisc.com>

--- sw/device/lib/crypto/drivers/kmac.c
+++ sw/device/lib/crypto/drivers/kmac.c
@@ -1,8 +1,5 @@
-// Copyright zeroRISC Inc.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
 // Copyright lowRISC contributors (OpenTitan project).
+// Copyright zeroRISC Inc.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
